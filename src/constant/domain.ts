04bb4367762e051822475538a7612b3845b56990
amrkallas <amrkallas6@gmail.com>
2024-01-01 01:13:20 +0300

    base url

--- src/constant/domain.ts
+++ src/constant/domain.ts
@@ -1,2 +1,5 @@
-export const API_BASE_URL="http://localhost:3000/api/v1"
+export const API_BASE_URL="https://ubay-9pwo.onrender.com"
 export const WEB_SITE_URL="http://localhost:5173"
+// https://ubay-9pwo.onrender.com/
+
+// "http://localhost:3000/api/v1"
