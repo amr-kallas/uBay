1098f7b2fe3184ecbe0ce2e77c7bb6160d6c25f8
amrkallas <amrkallas6@gmail.com>
2024-01-01 18:21:45 +0300

    url

--- src/constant/domain.ts
+++ src/constant/domain.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL="http://localhost:3000/api/v1"
+export const API_BASE_URL="https://ubay-9pwo.onrender.com/api/v1"
 export const WEB_SITE_URL="http://localhost:5173"
 // https://ubay-9pwo.onrender.com/api/v1
 
